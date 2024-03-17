94cc5fc379200b3216f14b2c905a572d198b7d87
Evgeny <bril424@mail.ru>
2024-03-17 22:52:44 +0100

    added components Footer Navigation and Logo

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,26 @@
 export default function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer className="bg-[#333333] text-white">
+			<div className="max-container flex items-start justify-around py-10 font-light">
+				<div>
+					<h1 className="py-2 text-4xl">KreaTec GmbH</h1>
+					<p>Rudolf-Diesel-Straße 7</p>
+					<p className="py-2 text-xl">72250 Freudenstadt</p>
+				</div>
+				<div>
+					<p className="py-2 text-xl duration-300 hover:text-blue-500">Telefon 07441 9245201</p>
+					<p className="py-2 text-xl">Fax 07441 9245203</p>
+					<p className="py-2 text-xl duration-300 hover:text-blue-500">
+						info(at)kreatec-bildung.de
+					</p>
+				</div>
+				<div>
+					<p className="py-2 text-xl duration-300 hover:text-blue-500">Seite drucken</p>
+					<p className="py-2 text-xl duration-300 hover:text-blue-500">Impressum</p>
+					<p className="py-2 text-xl duration-300 hover:text-blue-500">Datenschutz</p>
+					<p className="py-2 text-xl duration-300 hover:text-blue-500">by hitcom</p>
+				</div>
+			</div>
+		</footer>
+	);
 }
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,3 +1,25 @@
+import Logo from "../../assets/Logo.jpg";
+import { Link } from "react-router-dom";
+
 export default function Nav() {
-	return <div>Nav</div>;
+	return (
+		<header>
+			<nav className="max-container flex items-end justify-around pt-4">
+				<div>
+					<img src={Logo} alt="logo" className="p-4 shadow-lg" />
+				</div>
+				<div className="flex gap-10 text-2xl font-light ">
+					<Link className="p-4 shadow-lg transition-all duration-500 hover:bg-blue-800 hover:text-white hover:shadow-2xl hover:shadow-blue-900/70">
+						Über uns
+					</Link>
+					<Link className="p-4 shadow-lg transition-all duration-500 hover:bg-blue-800 hover:text-white hover:shadow-2xl hover:shadow-blue-900/70">
+						Maßnahmen & Projekte
+					</Link>
+					<Link className="p-4 shadow-lg transition-all duration-500 hover:bg-blue-800 hover:text-white hover:shadow-2xl hover:shadow-blue-900/70">
+						Aktuelles
+					</Link>
+				</div>
+			</nav>
+		</header>
+	);
 }
